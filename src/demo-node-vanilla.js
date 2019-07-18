ce662bbddbc15fa157c82a4db5e3646d14ee45e6
Robert Rypuła <robert.rypula@gmail.com>
2019-07-18 22:23:26 +0200

    Npm packages versions update. Bugfix in demo-node-vanilla

--- src/demo-node-vanilla.js
+++ src/demo-node-vanilla.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2019 Robert Rypuła - https://github.com/robertrypula
 
-const AudioNetworkLite = new require('../dist/audio-network-lite-v0.0.0.js');
+const AudioNetworkLite = require('../dist/audio-network-lite-v0.0.0.js');
 
 console.log(AudioNetworkLite);
