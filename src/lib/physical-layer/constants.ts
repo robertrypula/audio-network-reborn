44a2c71bcf61ade7739d468c3103f63f05cbcaf0
Robert Rypuła <robert.rypula@gmail.com>
2019-07-30 07:39:29 +0200

    Config experiments

--- src/lib/physical-layer/constants.ts
+++ src/lib/physical-layer/constants.ts
@@ -5,6 +5,6 @@ export const MILLISECONDS_IN_SECOND = 1000;
 export const NYQUIST_TWICE = 2;
 export const SILENCE_FREQUENCY = 0;
 
-export const FREQUENCY_08_KHZ = 8000;
-export const FREQUENCY_16_KHZ = 16000;
-export const FREQUENCY_20_KHZ = 20000;
+export const FREQUENCY_08_0_KHZ = 8000;
+export const FREQUENCY_16_0_KHZ = 16000;
+export const FREQUENCY_19_5_KHZ = 19500;
