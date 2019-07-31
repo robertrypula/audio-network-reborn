dff1274a77371981bb6374a54da9b70d645894c2
Robert Rypuła <robert.rypula@gmail.com>
2019-07-31 07:21:14 +0200

    Data frame implementation starts

---
+++ src/lib/data-link-layer/data-frame.spec.ts
@@ -0,0 +1,11 @@
+import { DataFrame } from './data-frame';
+
+describe('DataFrame', () => {
+  it('should return correct payload\'s length from header', () => {
+    const dataFrame = new DataFrame();
+    const payload = [10, 20, 30, 40];
+    
+    dataFrame.setPayload(payload);
+    expect(dataFrame.getLengthFromRawBytes()).toBe(payload.length);
+  });
+});
--- src/lib/data-link-layer/data-frame.ts
+++ src/lib/data-link-layer/data-frame.ts
@@ -1,55 +1,67 @@
 // Copyright (c) 2019 Robert Rypuła - https://github.com/robertrypula
 
-export class DataFrame {
-  protected rawBytes: number[];
-  protected rawBytePosition: number;
+import { getFletcher16 } from './utils';
 
-  public constructor(rawBytes: number[]) {
-    this.rawBytes = rawBytes.slice(0);
-    this.rawBytePosition = 0;
-  }
+export class DataFrame {
+  protected rawBytes: number[] = [];
+  protected rawBytePosition: number = 0;
 
   public getCalculatedChecksumFromPayload(): number {
     return 0x1234; // TODO implement
   }
 
   public getChecksumFromRawBytes(): number {
-    return 0x1234; // TODO implement
+    return this.rawBytes.length >= 2
+      ? ((this.rawBytes[0] << 8) | this.rawBytes[1]) & 0x1FFF
+      : null;
   }
   public getLengthFromPayload(): number {
-    return 1; // TODO implement
+    return this.rawBytes.length - 2;
   }
-
+  
   public getLengthFromRawBytes(): number {
-    return 1; // TODO implement
+    return this.rawBytes.length >= 1
+      ? ((this.rawBytes[0] >>> 5) & 0x07) + 1
+      : null;
   }
 
-  public getNextRawByte(): number | null {
+  public getNextRawByte(): number {
     return this.rawBytePosition < this.rawBytes.length
       ? this.rawBytes[this.rawBytePosition++]
       : null;
   }
 
   public getPayload(): number[] {
-    return this.rawBytes.slice(0); // TODO implement
+    return this.rawBytes.slice(2);
   }
 
   public getRawBytes(): number[] {
-    return this.rawBytes.slice(0); // TODO implement
+    return this.rawBytes;
   }
 
   public isValid(): boolean {
     return (
+      this.rawBytes.length > 2 &&
       this.getChecksumFromRawBytes() === this.getCalculatedChecksumFromPayload() &&
       this.getLengthFromRawBytes() === this.getLengthFromPayload()
     );
   }
 
   public setPayload(payload: number[]): void {
-    this.rawBytes = payload.slice(0); // TODO implement
+    const payloadLength = payload.length;
+    
+    if (payloadLength === 0 || payloadLength > 8) {
+      throw new Error('');
+    }
+    
+    this.rawBytes = [0, 0, ...payload];
+    this.rawBytePosition = 0;
+    // TODO mix everything into first two bytes
+    
   }
 
   public setRawBytes(rawBytes: number[]): void {
-    this.rawBytes = rawBytes.slice(0); // TODO implement
+    this.rawBytes = rawBytes;
+    this.rawBytePosition = 0;
   }
 }
