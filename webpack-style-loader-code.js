ec57e764165f76303eb858ac285848892f7c02f1
Robert Rypuła <robert.rypula@gmail.com>
2019-09-08 22:02:32 +0200

    Code simplifications in style-loder and updates in index.html

--- webpack-style-loader-code.js
+++ webpack-style-loader-code.js
@@ -5,8 +5,6 @@
 
 // TODO: simplify it even more and add AudioNetworkLite's copyrights
 
-const stylesInDom = {};
-
 function listToStyles(list) {
   const styles = [];
   const newStyles = {};
@@ -15,12 +13,11 @@ function listToStyles(list) {
     const item = list[i];
     const id = item[0];
     const css = item[1];
-    const part = { css };
 
     if (!newStyles[id]) {
-      styles.push((newStyles[id] = { id, parts: [part] }));
+      styles.push((newStyles[id] = { id, parts: [css] }));
     } else {
-      newStyles[id].parts.push(part);
+      newStyles[id].parts.push(css);
     }
   }
 
@@ -30,87 +27,26 @@ function listToStyles(list) {
 function addStylesToDom(styles) {
   for (let i = 0; i < styles.length; i++) {
     const item = styles[i];
-    const domStyle = stylesInDom[item.id];
-    let j = 0;
 
-    if (domStyle) {
-      domStyle.refs++;
-
-      for (; j < domStyle.parts.length; j++) {
-        domStyle.parts[j](item.parts[j]);
-      }
-
-      for (; j < item.parts.length; j++) {
-        domStyle.parts.push(addStyle(item.parts[j]));
-      }
-    } else {
-      const parts = [];
-
-      for (; j < item.parts.length; j++) {
-        parts.push(addStyle(item.parts[j]));
-      }
-
-      stylesInDom[item.id] = { id: item.id, refs: 1, parts };
+    for (let j = 0; j < item.parts.length; j++) {
+      addStyle(item.parts[j]);
     }
   }
 }
 
-function insertStyleElement() {
+function addStyle(css) {
   const style = document.createElement('style');
 
   document.querySelector('head').appendChild(style);
-
-  return style;
-}
-
-function removeStyleElement(style) {
-  if (style.parentNode === null) {
-    return false;
-  }
-
-  style.parentNode.removeChild(style);
-}
-
-function applyToTag(style, obj) {
-  let css = obj.css;
-
-  while (style.firstChild) {
-    style.removeChild(style.firstChild);
-  }
   style.appendChild(document.createTextNode(css));
-}
-
-function addStyle(obj) {
-  let style;
-  let update;
-  let remove;
-
-  style = insertStyleElement();
-
-  update = applyToTag.bind(null, style);
-  remove = () => {
-    removeStyleElement(style);
-  };
-
-  update(obj);
-
-  return function updateStyle(newObj) {
-    if (newObj) {
-      if (newObj.css === obj.css) {
-        return;
-      }
-
-      update((obj = newObj));
-    } else {
-      remove();
-    }
-  };
 }
 
 module.exports = (list, options) => {
   options = options || {};
 
-  if (typeof document !== 'undefined') { // solves the 'ReferenceError: document is not defined' on node environment
+  // solves the 'ReferenceError: document is not defined' on node environment
+  // TODO move 'audio-network-lite-root' check to options
+  if (typeof document !== 'undefined' && document.getElementById('audio-network-lite-root')) {
     addStylesToDom(listToStyles(list));
   }
 };
